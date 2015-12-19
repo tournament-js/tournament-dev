528a434f03bdf223b331ee240348dfaf82a49478
clux <analsandblaster@gmail.com>
2015-12-19 10:27:23 +0000

    implement tag test and a preliminary common interface test for sanity

--- test.js
+++ test.js
@@ -2,31 +2,33 @@
 var test = require('bandage');
 var join = require('path').join;
 var fs = require('fs');
+var cp = require('child_process');
 var semver = require('semver');
 var async = require('async');
 
-const deps = [
-  // core
-  'duel',
-  'duel-names',
-  'ffa',
+const base = ['tournament', 'tourney'];
+const tournaments = ['duel', 'ffa', 'groupstage', 'masters', 'tiebreaker'];
+const tourneys = [
   'ffa-tb',
-  'group',
-  'groupstage',
   'groupstage-tb',
   'groupstage-tb-duel',
-  'masters',
-  'roundrobin',
-  'tiebreaker',
-  'tournament',
-  'tourney',
-  // helpers
-  //'autonomy',
-  //'interlude',
-  //'operators',
-  //'smell',
-  //'subset',
-  //'sulfur'
+];
+const impls = tournaments.concat(tourneys)
+
+const helpers = [
+  'duel-names',
+  'group',
+  'roundrobin'
+]
+
+const deps = base.concat(impls).concat(helpers)
+const extras = [
+  'autonomy',
+  'interlude',
+  'operators',
+  'smell',
+  'subset',
+  'sulfur'
 ];
 
 var getJson = function (pth, cb) {
@@ -59,7 +61,23 @@ var getJsons = function (dir) {
   });
 };
 
-test('latest published', function *(t) {
+
+var latestTag = function (name, cb) {
+  return new Promise((res) => {
+    var cmd = 'cd ' + __dirname + '/' + name + ' && git tag -l | tail -n 1';
+    cp.exec(cmd, (err, out) => {
+      if (err) {
+        throw err;
+      }
+      var version = out.slice(0, -1); // slice away newline character in output
+      res({name, version});
+    });
+  });
+};
+
+
+// Versions should be up to date and depend on latest minor for sane dedupe bundling
+test('versions and tags', function *(t) {
   var jsons = yield getJsons(__dirname);
   var canonicals = jsons.reduce((acc, el) => {
     acc[el.data.name] = el.data.version;
@@ -70,6 +88,7 @@ test('latest published', function *(t) {
     t.ok(canonicals[dep], 'canonical version of ' + dep + ' is ' + canonicals[dep]);
   });
 
+  // Ensure all dependencies are stupidly up to date:
   jsons.forEach((json) => {
     Object.keys(json.data.dependencies).forEach((key) => {
       var qualifier = json.data.dependencies[key];
@@ -84,8 +103,22 @@ test('latest published', function *(t) {
       }
     });
   });
+
+  // Ensure we have actually published the up to date versions (tag present):
+  var tags = yield Array.from(deps.map(latestTag));
+  tags.forEach(el => {
+    t.ok(el.version, el.name + ' has a tag for ' + el.version);
+    var v = el.version.slice(1); // strip leading 'v'
+    t.equal(v, canonicals[el.name], el.name + ' latest tag is equal to its json ver');
+  });
 });
 
-// TODO: verify each class outputs an `Id` class
-// TODO: verify each class has a tag equal to the version the package.json is at
-
+// Some things not enforced by class hierarchy, but should for consistency:
+test('Common interface', function *(t) {
+  tournaments.forEach(m => {
+    var Klass = require('./' + m + '/');
+    // Each tournament class should export an `Id` class
+    // TODO: need this be uniformly true for tourneys? - should we export it for bases?
+    t.type(Klass.Id, 'function', m + ' has Id class exposed');
+  });
+});
